84d13e0ada50ae702771e6faa3a7887aaf739e49
Mohammad Arshad <mdarshad1000@gmail.com>
2022-10-27 04:05:01 +0530

    Change domain name

--- codeblocks/settings.py
+++ codeblocks/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS=["https://codebook.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS=["https://codebook.up.railway.app"]
 
 # Application definition
 
