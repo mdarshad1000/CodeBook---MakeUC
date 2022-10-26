146695a0b86243c974585c042af9ba0c52ef0586
Mohammad Arshad <mdarshad1000@gmail.com>
2022-10-27 03:15:01 +0530

    Add CSRF

--- codeblocks/settings.py
+++ codeblocks/settings.py
@@ -29,8 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS=[
-    "https://codebook-makeuc-production.up.railway.app/login",
-    "https://codebook-makeuc-production.up.railway.app/login"
+    "https://codebook-makeuc-production.up.railway.app/login/",
+    "https://codebook-makeuc-production.up.railway.app/register/"
     ]
 # Application definition
 
